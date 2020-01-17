5662177b5683f1c59c1a8524238c1d89ab39a6d3
sophielihenry <sophiesophiehenry@googlemail.com>
2020-01-17 16:04:55 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+    	height: {
+    		'800': '800px',
+    	},
+    	colors: {
+    		'green': '#A8C256',
+    	},
+    	fontFamily: {
+  			'standard': 'Gothic A1',
+  		},
+      fontSize: {
+        'big': '53px',
+      },
+    }
+  },
+  variants: {},
+  plugins: []
+}
